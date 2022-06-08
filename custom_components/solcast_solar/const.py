476e36b34c0a896b9cbe6065bee5ae8e41af1068
Greg <kiza.soza@gmail.com>
2022-06-08 17:25:35 +1000

    Update const.py

--- custom_components/solcast_solar/const.py
+++ custom_components/solcast_solar/const.py
@@ -4,8 +4,7 @@ from __future__ import annotations
 from typing import Final
 
 DOMAIN = "solcast_solar"
-#SOLCAST_URL = "https://api.solcast.com.au"
-SOLCAST_URL = "http://10.0.1.20"
+SOLCAST_URL = "https://api.solcast.com.au"
 
 ATTR_ENTRY_TYPE: Final = "entry_type"
 ENTRY_TYPE_SERVICE: Final = "service"
